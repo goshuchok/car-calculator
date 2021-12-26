65ec6813eef16dccdb6983538edaae50d59134f8
Igor Povar <goshuchok@gmail.com>
2021-12-26 15:00:56 +0200

    for modile hover component

--- src/components/header/Phone.js
+++ src/components/header/Phone.js
@@ -1,11 +1,21 @@
+import { makeStyles } from '@material-ui/core';
 import phone from '../../images/phone.png';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    [theme.breakpoints.only('xs')]: {
+      display: 'none',
+    },
+  },
+}));
+
 function Phone() {
+  const classes = useStyles();
   return (
     <div className="phone">
-      <img className="phone__icons" alt="phone" src={phone}></img>
       <a className="phone__links" href="tel:+79946643627">
-        +7 994-664-36-27
+        <img className="phone__icons" alt="phone" src={phone}></img>
+        <p className={classes.root}>+7 994-664-36-27</p>
       </a>
     </div>
   );
