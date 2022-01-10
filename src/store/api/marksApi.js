4f7d0b70e8b3efa4a289bd7f4e70aea0b3a6b57f
Igor Povar <goshuchok@gmail.com>
2022-01-10 23:12:50 +0200

    optimization actions

--- src/store/api/marksApi.js
+++ src/store/api/marksApi.js
@@ -1,13 +1,9 @@
 import axios from 'axios';
 import { API_URL } from '../config';
 
-const key = 'xMjRSheumFDOR3KhqDuV81Ytp9OZSAhRpQRsZo5A';
-const API_MARKS = '/auto/categories';
-// const MARKS_ID = 1;
-export const getMarks = (MARKS_ID = 1) => {
-  // return axios.get(`${API_URL}${API_MARKS}/${id}/marks?api_key=${key}`);
-  return axios.get(`${API_URL}${API_MARKS}${MARKS_ID}/marks?api_key=${key}`);
-  // return axios.get(`${API_URL}/auto/info?api_key=${key}&auto_id=19050985`);
-};
+export const key = 'xMjRSheumFDOR3KhqDuV81Ytp9OZSAhRpQRsZo5A';
+export const categoriesUrl = '/auto/categories';
 
-export default API_MARKS;
+export const getMarks = (marksId = 1) => {
+  return axios.get(`${API_URL}${categoriesUrl}${marksId}/marks?api_key=${key}`);
+};
--- src/store/api/uniqueApi.js
+++ src/store/api/uniqueApi.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { API_URL } from '../config';
-
-const key = 'xMjRSheumFDOR3KhqDuV81Ytp9OZSAhRpQRsZo5A';
+import { key } from './marksApi';
 
 const UNIQUE_ID = 19050985;
 export const getUnique = () => {
